a5030d793dc4bb76788bc8ecf07ca97e2b55c4f8
lideyu <lideyu@haodf.com>
2024-01-06 17:02:18 +0800

    feat(ADD): FIX

--- biz/domain/service/data_craw_service.go
+++ biz/domain/service/data_craw_service.go
@@ -1,9 +1,4 @@
 package service
 
-import "speedy/read/biz/domain/aggregates/article_summary"
-
-type DataCrawingServiceI interface {
-	CreateArticleSummary(ctx context.Context, articleSummaryDO *article_summary.ArticleSummary) (int64, error)
-	GetArticleSummaryList(ctx context.Context, params article_summary.SummaryListParams) (
-		resp []*article_summary.ArticleSummary, err error)
+type DataCrawServiceI interface {
 }
--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"github.com/cloudwego/kitex/pkg/klog"
-	"github.com/cloudwego/kitex/server"
 	"log"
 	"net"
 	"os"
+
 	"speedy/read/biz/infra"
 	speedyRead "speedy/read/kitex_gen/speedy_read/speedyread"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/server"
 )
 
 func main() {
+	logInit()
 	// init infra
 	infra.Init()
-	logInit()
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:3000")
 	svr := speedyRead.NewServer(new(SpeedyReadImpl), server.WithServiceAddr(addr))
 	err := svr.Run()
