f1f9cb58c10c8c333e9ae30fd0b982513df31d6c
lideyu <lideyu@haodf.com>
2024-01-06 16:06:43 +0800

    feat(add): add log

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/server"
 	"log"
 	"net"
+	"os"
 	"speedy/read/biz/infra"
 	speedyRead "speedy/read/kitex_gen/speedy_read/speedyread"
 )
@@ -11,10 +13,20 @@ import (
 func main() {
 	// init infra
 	infra.Init()
+	logInit()
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:3000")
 	svr := speedyRead.NewServer(new(SpeedyReadImpl), server.WithServiceAddr(addr))
 	err := svr.Run()
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+
+func logInit() {
+	f, err := os.OpenFile("./output.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	klog.SetOutput(f)
 }
